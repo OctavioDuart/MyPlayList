d108ebc9de5aef45983f5d1f1af1f1942add7eaf
OctavioDuart <36246332+OctavioDuart@users.noreply.github.com>
2018-12-12 17:17:36 -0200

    Insert Options (Delete And Put)

--- Client-Side/src/components/table/table.js
+++ Client-Side/src/components/table/table.js
@@ -5,6 +5,8 @@ import './style_table.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import {BootstrapTable,TableHeaderColumn} from 'react-bootstrap-table';
 import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css'
+import img_delete from '../../img/delete.png'
+import img_update from '../../img/update.png'
 
 
 
@@ -30,6 +32,7 @@ export default class Table extends Component {
        @  fonte: https://stackoverflow.com/questions/49600249/reactjs-cannot-read-property-setstate-of-undefined
     */
 
+    //@Pega as musicas
     getPlayList =  () => {
         get((result) => {     
             if (result) 
@@ -38,13 +41,37 @@ export default class Table extends Component {
         });
     };
 
+    //@Deleta
+    handleClick = () =>{
+        console.log("Teste")
+    }
+
     render(){
+
+        class Options extends Component {
+            render() {
+              return (
+                <div>
+                    <img id="img-delete" src={img_delete}  title="Deletar Música" onClick={this.handleClick}  /> 
+                    <img id="img-update" src={img_update}  title="Alterar Dados"/>
+                </div>
+              );
+            }
+          }
+          
+          function activeFormatter(cell, row, enumObject, index) {
+            return (
+              <Options active={ cell } />
+            );
+          }
+
         return(          
-                <BootstrapTable data={this.state.playlist} keyField='_id' bordered={false} search={true} striped >
+                <BootstrapTable data={this.state.playlist} keyField='_id' bordered={false} search={true}  striped >
                         <TableHeaderColumn dataField='music'>Name</TableHeaderColumn>
                         <TableHeaderColumn dataField='single_band'>Band or Sigle</TableHeaderColumn>
                         <TableHeaderColumn dataField='album'>Album</TableHeaderColumn>
                         <TableHeaderColumn dataField='link'>Listen</TableHeaderColumn>
+                        <TableHeaderColumn dataField='active' dataFormat={ activeFormatter }>Options</TableHeaderColumn>
                 </BootstrapTable>
         )
     }
